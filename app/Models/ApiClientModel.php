19813a70e8b3ef8ec4065dbeab516a4ecf872549
devunplugged <66313338+devunplugged@users.noreply.github.com>
2022-04-21 15:08:25 +0200

    restrict access to locker list

--- app/Models/ApiClientModel.php
+++ app/Models/ApiClientModel.php
@@ -148,7 +148,12 @@ class ApiClientModel extends Model
     public function getCompanyAccessLockers($companyId)
     {
         //return $this->where('company_id', $companyId)->where('type', 'locker')->find();
-        return $this->join('lockeraccesses', 'lockeraccesses.locker_id = apiclients.id')->where('apiclients.type', 'locker')->where('apiclients.id',$companyId)->find();
+        $lockers = $this->join('lockeraccesses', 'lockeraccesses.locker_id = apiclients.id')->where('apiclients.type', 'locker')->where('apiclients.id', $companyId)->find();
+        $lockersList = [];
+        foreach($lockers as $locker){
+            $lockersList[] = new \App\Entities\ApiClient($locker);
+        }
+        return $lockersList;
     }
 
     public function getAllLockers()
